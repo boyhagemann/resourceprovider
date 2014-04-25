4188213059ffefeeffc18876abada4aff22ef8ed
= <=>
2014-04-25 16:54:42 +0200

    update view

--- app/views/news-index.blade.php
+++ app/views/news-index.blade.php
@@ -1,8 +1,15 @@
+
 <h1>{{{ $title or "News" }}}</h1>
 
 @foreach($news as $item)
-	<article>
-		<header><h1>{{{ $item['title'] }}}</h1></header>
-		<p>{{ $item['body'] }}</p>
+
+	<article class="media">
+		<a class="pull-left" href="#">
+
+		</a>
+		<div class="media-body">
+			<h4 class="media-heading">{{{ $item['title'] }}}</h4>
+			<p>{{ $item['body'] }}</p>
+		</div>
 	</article>
 @endforeach
